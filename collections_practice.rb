5da2ded8d3cafa8f05ed752de044c62a80b5f77f
Levi Borland <borland.levi@gmail.com>
2019-08-18 23:33:08 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,14 +6,8 @@ def sort_array_desc(integers)
   integers.sort.reverse
 end
 
-sort_array_char_count(strings)
+def sort_array_char_count(strings)
   strings.sort do |a, b|
     a.length <=> b.length
-  end
-end
-
-def sort_array_char_count(array)
-  array.sort do |left, right|
-    left.length <=> right.length
   end
 end
