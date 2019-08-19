ebae5619b0b674ba6cfe99ca7517ee28496669a2
Levi Borland <borland.levi@gmail.com>
2019-08-19 00:56:45 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -47,12 +47,6 @@ def sum_array(integers)
 end
 
 def add_s(array)
-  array.each_with_index do |word, index|
-    if index != 1 
-      word = "#{word}s"
-    else
-      word = word
-    end
-  end
+  array.collect do |word|
 end
 
